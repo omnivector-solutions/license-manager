77f7e36cd3e09ad8aceb021ee52078052141321d
Cory Dodt <121705+corydodt@users.noreply.github.com>
2021-01-15 12:17:35 -0800

    some basic repo setup

--- Makefile
+++ Makefile
@@ -1,2 +1,19 @@
+SHELL				:= /bin/bash
+
+
 lint: ## Run linter
-	@tox -e lint
+	tox -e lint
+
+
+requirements.txt: setup.py
+	python3 -m venv _virtual_tmp
+	. _virtual_tmp/bin/activate \
+		&& pip install wheel \
+		&& pip install . \
+		&& pip freeze | grep -v license-manager > requirements.txt
+	rm -rf _virtual_tmp
+
+
+format: # reformat source python files
+	isort license_manager setup.py
+	black license_manager setup.py
--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
-import setuptools
 import sys
+
+import setuptools
+
 
 sys.path.append(".")
 
-from license_manager import VERSION # NOQA
+from license_manager import VERSION  # NOQA
 
 
 setuptools.setup(
-    name='license-manager',
+    name="license-manager",
     version=VERSION,
-    packages=setuptools.find_packages(
-        include=["*"]
-    ),
-    license='GPLv3',
-    long_description=open('README.md').read(),
-    install_requires=['pyyaml', 'sentry-sdk'],
+    packages=setuptools.find_packages(include=["license_manager", "license_manager.*"]),
+    license="GPLv3",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["pyyaml", "sentry-sdk"],
+    extras_require={
+        "dev": ["black", "flake8", "isort", "pytest", "pytest-cov", "tox", "wheel"]
+    },
     entry_points={
-        'console_scripts': [
-            'license-server=license_manager.server.main:main',
-            'license-agent=license_manager.agent.main:main',
+        "console_scripts": [
+            "license-server=license_manager.server.main:main",
+            "license-agent=license_manager.agent.main:main",
         ],
     },
     include_package_data=True,
-    python_requires='>=3.6.8',
+    python_requires=">=3.6.8",
 )
