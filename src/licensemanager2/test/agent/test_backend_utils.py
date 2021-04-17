f6be7261100fdba7c222b3837562c1434a112b87
jamesbeedy <jamesbeedy@gmail.com>
2021-04-17 12:49:45 -0700

    deduplicate the code that get the config path
    These changes add a function to the test_backend_utils to
    generate the config file path and make the bad config yaml file
    invalid yaml.

--- src/licensemanager2/test/agent/test_backend_utils.py
+++ src/licensemanager2/test/agent/test_backend_utils.py
@@ -16,13 +16,21 @@ def license_server_features() -> List[Any]:
     return [{"features": ["TESTFEATURE"], "license_server_type": "flexlm"}]
 
 
+def get_license_server_feature_config_file(filename):
+    """
+    Return the license_server_feature_config_file path as a string.
+    """
+    path = os.path.abspath(__file__)
+    dir_path = os.path.dirname(path)
+    return os.path.join(dir_path, f"test_configs/{filename}")
+
+
 def test_get_license_server_features():
     """
     Do I produce the correct license server/features config?
     """
-    path = os.path.abspath(__file__)
-    dir_path = os.path.dirname(path)
-    license_server_features_config_path = os.path.join(dir_path, "test_configs/license_server_config.yaml")
+    config_file_name = "license_server_config.yaml"
+    license_server_features_config_path = get_license_server_feature_config_file(config_file_name)
     # Patch the objects needed to generate a report.
     p1 = patch.object(settings.SETTINGS, "LICENSE_SERVER_FEATURES_CONFIG_PATH", license_server_features_config_path)
     with p1:
@@ -31,11 +39,10 @@ def test_get_license_server_features():
 
 def test_bad_license_server_features_config():
     """
-    Do I produce the correct config when a non existent config file is passed?
+    Do I produce the correct config when a non-valid config file is passed?
     """
-    path = os.path.abspath(__file__)
-    dir_path = os.path.dirname(path)
-    license_server_features_config_path = os.path.join(dir_path, "test_configs/license_server_config_bad.yaml")
+    config_file_name = "license_server_config_bad.yaml"
+    license_server_features_config_path = get_license_server_feature_config_file(config_file_name)
     # Patch the objects needed to generate a report.
     p1 = patch.object(settings.SETTINGS, "LICENSE_SERVER_FEATURES_CONFIG_PATH", license_server_features_config_path)
     with p1:
