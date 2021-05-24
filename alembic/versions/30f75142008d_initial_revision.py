56b09e8de0a76d1822cbe41931056a318a932fa4
djkresta <david@omnivector.solutions>
2021-05-24 18:38:58 +0000

    add alembic migrations
    These changes introduce the alembic.ini file and initial alembic
    migration.

---
+++ alembic/versions/30f75142008d_initial_revision.py
@@ -0,0 +1,24 @@
+"""initial revision
+
+Revision ID: 30f75142008d
+Revises: 
+Create Date: 2021-05-24 18:10:48.718930
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '30f75142008d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
