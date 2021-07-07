32b61ec3b3abe6ce700b0ef4988dceb6eab094df
djkresta <david@omnivector.solutions>
2021-07-07 21:03:04 +0000

    first pass at adding foreign key to config table into the booking table

--- src/licensemanager2/backend/schema.py
+++ src/licensemanager2/backend/schema.py
@@ -1,9 +1,12 @@
 """
 Table schema for tables in the license-manager backend
 """
+import datetime
 import sqlalchemy
-from sqlalchemy import Integer, String
-from sqlalchemy.sql.schema import CheckConstraint, Column
+
+from sqlalchemy import Column, Integer, String, Table
+from sqlalchemy.sql.schema import CheckConstraint, ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
 
 from sqlalchemy_utils import ScalarListType  # type: ignore
 
@@ -11,25 +14,35 @@ from sqlalchemy_utils import ScalarListType  # type: ignore
 metadata = sqlalchemy.MetaData()
 
 
-license_table = sqlalchemy.Table(
+license_table = Table(
     "license",
     metadata,
+    Column("id", Integer, primary_key=True, nullable=True),
     Column("product_feature", String, primary_key=True),
     Column("used", Integer, CheckConstraint("used>=0")),
     Column("total", Integer, CheckConstraint("total>=0")),
     CheckConstraint("used<=total"),
+    sqlite_autoincrement=True
 )
 
 
-booking_table = sqlalchemy.Table(
+booking_table = Table(
     "booking",
     metadata,
-    Column("job_id", String, primary_key=True),
-    Column("product_feature", String, primary_key=True),
+    Column("id", Integer, primary_key=True),
+    Column("job_id", String),
+    Column("product_feature", String),
     Column("booked", Integer, CheckConstraint("booked>=0")),
+    Column("lead_host", String),
+    Column("user_name", String),
+    Column("cluster_name", String),
+    Column("created_at", DateTime, default=datetime.datetime.utcnow),
+    Column('config_id', Integer, ForeignKey("config.id"), nullable=True),
+    sqlite_autoincrement=True
 )
 
-config_table = sqlalchemy.Table(
+
+config_table = Table(
     "config",
     metadata,
     Column("id", Integer, primary_key=True),
@@ -38,4 +51,5 @@ config_table = sqlalchemy.Table(
     Column("license_servers", ScalarListType(str)),
     Column("license_server_type", String),
     Column("grace_time", Integer, CheckConstraint("grace_time>=0")),
+    sqlite_autoincrement=True
 )
