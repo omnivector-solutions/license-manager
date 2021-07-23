c7f9693a3f82161a54c89331b35c8d3b39dc5cc9
jamesbeedy <jamesbeedy@users.noreply.github.com>
2021-07-23 09:29:56 -0700

    Update src/licensemanager2/test/workload_managers/slurm/cmd_test_utils.py

--- src/licensemanager2/test/workload_managers/slurm/cmd_test_utils.py
+++ src/licensemanager2/test/workload_managers/slurm/cmd_test_utils.py
@@ -8,5 +8,11 @@ def test_squeue_parser_returns_correct_output_format():
         {"job_id": 2, "run_time_in_seconds": 184981, "state": "RUNNING"},
         {"job_id": 3, "run_time_in_seconds": 17084, "state": "RUNNING"},
     ]
-    squeue_parsed = squeue_parser("1|5:00|RUNNING\n2|2-3:23:01|RUNNING\n3|4:44:44|RUNNING")
+    squeue_parsed = squeue_parser(
+        "\n".join([
+            "1|5:00|RUNNING",
+            "2|2-3:23:01|RUNNING",
+            "3|4:44:44|RUNNING",
+        ])
+    )
     assert squeue_parsed == squeue_parsed_output
