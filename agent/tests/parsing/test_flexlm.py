c35cc80b6cd9b4750637666b169534bf6216b48d
zsinx6 <zsinx6@users.noreply.github.com>
2021-08-17 16:05:32 -0300

    Parser now returns lead_host, user_name and booked

--- agent/tests/parsing/test_flexlm.py
+++ agent/tests/parsing/test_flexlm.py
@@ -1,58 +1,26 @@
 """
 Test the flexlm parser
 """
-from textwrap import dedent
-
-from pytest import fixture, mark
+from pytest import mark
 
 from lm_agent.parsing.flexlm import parse
-
-
-@fixture
-def lm_output_bad():
-    """
-    Some unparseable lmstat output
-    """
-    return dedent(
-        """\
-    lmstat - Copyright (c) 1989-2004 by Macrovision Corporation. All rights reserved.
-    Flexible License Manager status on Wed 03/31/2021 09:12
-
-    Error getting status: Cannot connect to license server (-15,570:111 "Connection refused")
-    """
-    )
-
-
-@fixture
-def lm_output():
-    """
-    Some lmstat output to parse
-    """
-    return dedent(
-        """\
-        lmstat - Copyright (c) 1989-2004 by Macrovision Corporation. All rights reserved.
-        ...
-
-        Users of TESTFEATURE:  (Total of 1000 licenses issued;  Total of 93 licenses in use)
-
-        ...
-
-
-        """
-        "           jbemfv myserver.example.com /dev/tty (v62.2) (myserver.example.com/24200 12507), "
-        "start Thu 10/29 8:09, 29 licenses\n"
-        "           cdxfdn myserver.example.com /dev/tty (v62.2) (myserver.example.com/24200 12507), "
-        "start Thu 10/29 8:09, 27 licenses\n"
-        "           jbemfv myserver.example.com /dev/tty (v62.2) (myserver.example.com/24200 12507), "
-        "start Thu 10/29 8:09, 37 licenses\n"
-    )
 
 
 @mark.parametrize(
     "fixture,result",
     [
-        ("lm_output", {"feature": "TESTFEATURE", "total": 1000, "used": 93}),
-        ("lm_output_bad", {}),
+        (
+            "lm_output",
+            {
+                "total": {"feature": "TESTFEATURE", "total": 1000, "used": 93},
+                "uses": [
+                    {"booked": 29, "lead_host": "myserver.example.com", "user_name": "jbemfv"},
+                    {"booked": 27, "lead_host": "myserver.example.com", "user_name": "cdxfdn"},
+                    {"booked": 37, "lead_host": "myserver.example.com", "user_name": "jbemfv"},
+                ],
+            },
+        ),
+        ("lm_output_bad", {"total": None, "uses": []}),
     ],
 )
 def test_parse(request, fixture, result):
