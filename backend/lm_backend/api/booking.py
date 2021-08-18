c924c0cdcbb2e7948e22faa4ddeac5f2ea105331
zsinx6 <zsinx6@users.noreply.github.com>
2021-08-18 12:43:22 -0300

    Fix typo

--- backend/lm_backend/api/booking.py
+++ backend/lm_backend/api/booking.py
@@ -74,8 +74,8 @@ async def create_booking(booking: Booking):
         )
 
     inserts_without_id = []
-    for booking in inserts:
-        inserts_without_id.append(booking.dict(exclude={"id"}))
+    for insert in inserts:
+        inserts_without_id.append(insert.dict(exclude={"id"}))
     try:
         await database.execute_many(query=booking_table.insert(), values=[i for i in inserts_without_id])
     except INTEGRITY_CHECK_EXCEPTIONS as e:
