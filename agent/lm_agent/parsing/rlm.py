a629292cb17e51a72fd7530d048000171391aba6
zsinx6 <zsinx6@users.noreply.github.com>
2021-11-03 12:52:15 -0300

    Get start offset for rlm

--- agent/lm_agent/parsing/rlm.py
+++ agent/lm_agent/parsing/rlm.py
@@ -9,7 +9,7 @@ HOSTWORD = r"[a-zA-Z0-9-]+"
 HOSTWORD2 = r"[a-zA-Z0-9-.]+"
 HOSTNAME = rf"{HOSTWORD}(\.{HOSTWORD2})*"
 
-TOTALS_LINE = rf"^(?P<license_feature>\w+) {VERSION}: (?P<user_name>\w+)@(?P<lead_host>{HOSTNAME}) (?P<license_used_by_host>\d+).*$"
+TOTALS_LINE = rf"^(?P<license_feature>\w+) {VERSION}: (?P<user_name>\w+)@(?P<lead_host>{HOSTNAME}) (?P<license_used_by_host>\d+).*$"  # noqa
 DATA_LINE = rf"^\s*(?P<license_feature>\w+) {VERSION}$"
 COUNT_LINE = r"^\s*count: (?P<count>\d+).*inuse: (?P<in_use>\d+).*$"
 
@@ -18,14 +18,28 @@ RX_DATA = re.compile(rf"{DATA_LINE}")
 RX_COUNT = re.compile(rf"{COUNT_LINE}")
 
 
+def _get_start_offset(lines) -> int:
+    """Get the start offset of the license data."""
+    i = len(lines) - 1
+    count = 0
+    for line in lines[::-1]:
+        if "-" * 10 in line:
+            count += 1
+        if count == 2:
+            break
+        i -= 1
+    return i
+
+
 def parse(s: str) -> dict:
     """
     Parse lines of the license output with regular expressions
     """
     parsed_dict: dict = {"uses": [], "total": []}
-    lines = [line.strip() for line in s.splitlines()]
+    start_offset = _get_start_offset(s.splitlines())
+    lines = [line.strip() for line in s.splitlines()[start_offset:]]
 
-    for i, line in enumerate(lines[45:]):
+    for i, line in enumerate(lines):
         parsed_total = RX_TOTAL.match(line)
         parsed_data = RX_DATA.match(line)
         if parsed_total:
