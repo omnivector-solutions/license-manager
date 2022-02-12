a1bb1bfbe88c70f15ec534a21136a3cec34dc813
Juliana Karoline <julianaklulo@gmail.com>
2022-02-11 23:37:21 -0300

    Add LS-Dyna report tests to tokenstat tests

--- agent/tests/test_tokenstat.py
+++ agent/tests/test_tokenstat.py
@@ -9,28 +9,6 @@ from lm_agent.backend_utils import BackendConfigurationRow
 
 
 @fixture
-def one_configuration_row_flexlm():
-    return BackendConfigurationRow(
-        product="testproduct",
-        features={"testfeature": 10},
-        license_servers=["flexlm:127.0.0.1:2345"],
-        license_server_type="flexlm",
-        grace_time=10000,
-    )
-
-
-@fixture
-def one_configuration_row_rlm():
-    return BackendConfigurationRow(
-        product="converge",
-        features={"super": 10},
-        license_servers=["rlm:127.0.0.1:2345"],
-        license_server_type="rlm",
-        grace_time=10000,
-    )
-
-
-@fixture
 def scontrol_show_lic_output_flexlm():
     return dedent(
         """
@@ -46,6 +24,16 @@ def scontrol_show_lic_output_rlm():
         """
         LicenseName=converge.super@rlm
             Total=10 Used=0 Free=10 Reserved=0 Remote=yes
+        """
+    )
+
+
+@fixture
+def scontrol_show_lic_output_lsdyna():
+    return dedent(
+        """
+        LicenseName=mppdyna.mppdyna@lsdyna
+            Total=500 Used=0 Free=500 Reserved=0 Remote=yes
         """
     )
 
@@ -180,6 +168,67 @@ async def test_rlm_get_report(
 
 
 @mark.asyncio
+@mark.parametrize(
+    "output,reconciliation",
+    [
+        (
+            "lsdyna_output",
+            [
+                {
+                    "product_feature": "mppdyna.mppdyna",
+                    "used": 440,
+                    "total": 500,
+                    "used_licenses": [
+                        {"user_name": "fane8y", "lead_host": "n-c13.maas.rnd.com", "booked": 80},
+                        {"user_name": "ssskmj", "lead_host": "n-c52.maas.rnd.com", "booked": 80},
+                        {"user_name": "ssskmj", "lead_host": "n-c15.maas.rnd.com", "booked": 80},
+                        {"user_name": "ywazrn", "lead_host": "n-c53.maas.rnd.com", "booked": 80},
+                        {"user_name": "ywazrn", "lead_host": "n-c51.maas.rnd.com", "booked": 80},
+                        {"user_name": "ndhtw9", "lead_host": "n-c55.maas.rnd.com", "booked": 40},
+                    ],
+                },
+            ],
+        ),
+        (
+            "lsdyna_output_no_licenses",
+            [
+                {
+                    "product_feature": "mppdyna.mppdyna",
+                    "used": 0,
+                    "total": 500,
+                    "used_licenses": [],
+                },
+            ],
+        ),
+    ],
+)
+@mock.patch("lm_agent.server_interfaces.lsdyna.LSDynaLicenseServer.get_output_from_server")
+@mock.patch("lm_agent.tokenstat.scontrol_show_lic")
+@mock.patch("lm_agent.tokenstat.get_config_from_backend")
+async def test_lsdyna_get_report(
+    get_config_from_backend_mock: mock.MagicMock,
+    show_lic_mock: mock.MagicMock,
+    get_output_from_server_mock: mock.MagicMock,
+    output,
+    reconciliation,
+    one_configuration_row_lsdyna,
+    scontrol_show_lic_output_lsdyna,
+    request,
+):
+    """
+    Do I get a report for the LS-Dyna licenses in the cluster?
+    """
+    get_config_from_backend_mock.return_value = [one_configuration_row_lsdyna]
+    show_lic_mock.return_value = scontrol_show_lic_output_lsdyna
+
+    output = request.getfixturevalue(output)
+    get_output_from_server_mock.return_value = output
+
+    reconcile_list = await tokenstat.report()
+    assert reconcile_list == reconciliation
+
+
+@mark.asyncio
 @mock.patch("lm_agent.tokenstat.scontrol_show_lic")
 @mock.patch("lm_agent.tokenstat.get_config_from_backend")
 async def test_flexlm_report_with_empty_backend(
@@ -210,6 +259,24 @@ async def test_rlm_report_with_empty_backend(
     """
     get_config_from_backend_mock.return_value = []
     show_lic_mock.return_value = scontrol_show_lic_output_rlm
+
+    reconcile_list = await tokenstat.report()
+    assert reconcile_list == []
+
+
+@mark.asyncio
+@mock.patch("lm_agent.tokenstat.scontrol_show_lic")
+@mock.patch("lm_agent.tokenstat.get_config_from_backend")
+async def test_lsdyna_report_with_empty_backend(
+    get_config_from_backend_mock: mock.MagicMock,
+    show_lic_mock: mock.MagicMock,
+    scontrol_show_lic_output_lsdyna,
+):
+    """
+    Do I collect the requested structured data when the backend is empty?
+    """
+    get_config_from_backend_mock.return_value = []
+    show_lic_mock.return_value = scontrol_show_lic_output_lsdyna
 
     reconcile_list = await tokenstat.report()
     assert reconcile_list == []
