9c0a4e8971f9866cdb4ad3cc766f7835951a6b13
Juliana Karoline <julianaklulo@gmail.com>
2022-02-14 15:09:59 -0300

    Change logic to get group usage of licenses

--- agent/lm_agent/parsing/lsdyna.py
+++ agent/lm_agent/parsing/lsdyna.py
@@ -22,11 +22,20 @@ PROGRAM_LINE = (
 )
 
 USAGE_LINE = (
-    rf"\s+" rf"(?P<user>\S+)\s+" rf"(?P<port>\d+)\@" rf"(?P<lead_host>{HOSTNAME})\s+" rf"(?P<used>\d+)"
+    rf"\s+" rf"(?P<user>\S+)\s+" rf"(?P<port>{INT})\@" rf"(?P<lead_host>{HOSTNAME})\s+" rf"(?P<used>{INT})"
+)
+
+GROUP_LINE = (
+    rf"\s+LICENSE GROUP\s+"
+    rf"(?P<used>{INT})\s+"
+    rf"(?P<free>{INT})\s+"
+    rf"(?P<max>{INT})\s\|\s+"
+    rf"(?P<queue>{INT})"
 )
 
 RX_PROGRAM = re.compile(PROGRAM_LINE)
 RX_USAGE = re.compile(USAGE_LINE)
+RX_TOTAL = re.compile(GROUP_LINE)
 
 
 def parse(s: str) -> dict:
@@ -34,6 +43,8 @@ def parse(s: str) -> dict:
     Parse lines of the license output with regular expressions
     """
     parsed_data: dict = {}
+    group_used: int = 0
+
     lines = s.splitlines()
 
     for i, line in enumerate(lines):
@@ -63,9 +74,15 @@ def parse(s: str) -> dict:
                         "booked": int(usage_data["used"]),
                     }
                 )
-                calculated_used += int(usage_data["used"])
-            parsed_data[program_data["program"]]["used"] = calculated_used
-        else:
-            parsed_data[program_data["program"]]["used"] = int(program_data["used"])
+
+        parsed_total_usage = RX_TOTAL.match(line)
+        if parsed_total_usage is None:
+            continue
+
+        total_usage_data = parsed_total_usage.groupdict()
+        group_used = int(total_usage_data["used"])
+
+    for program in parsed_data.keys():
+        parsed_data[program]["used"] = group_used
 
     return parsed_data
