c61d12129f95abd3bf11b90ee5f81584949f3f73
Juliana Karoline <julianaklulo@gmail.com>
2022-03-08 16:23:42 -0300

    Remove left over line

--- agent/lm_agent/parsing/lmx.py
+++ agent/lm_agent/parsing/lmx.py
@@ -30,8 +30,7 @@ def parse_feature_line(line: str):
     if parsed_feature is None:
         return None
     feature_data = parsed_feature.groupdict()
-    feature_data["feature"] = feature_data["feature"]
-
+    
     return feature_data["feature"]
 
 
