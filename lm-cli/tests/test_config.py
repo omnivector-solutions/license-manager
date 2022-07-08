61c1d58238bfb539572a0872a41333a98cbdad9f
Juliana Karoline <julianaklulo@gmail.com>
2022-07-08 17:24:37 -0300

    Add tests for common parts of the project

---
+++ lm-cli/tests/test_config.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+
+from lm_cli.config import build_settings
+
+
+def test_Validation_error__when_parameter_is_missing():
+
+    original_value = os.environ.get("AUTH0_DOMAIN")
+    try:
+        if "AUTH0_DOMAIN" in os.environ:
+            del os.environ["AUTH0_DOMAIN"]
+
+        with pytest.raises(SystemExit) as e:
+            build_settings(_env_file=None)
+
+        assert e.value.code == 1
+    finally:
+        if original_value is not None:
+            os.environ["AUTH0_DOMAIN"] = original_value
