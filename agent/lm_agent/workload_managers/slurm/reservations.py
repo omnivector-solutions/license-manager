5678e125b6e0b6b2b388c6896e13c6b13b2283ea
Juliana Karoline <julianaklulo@gmail.com>
2023-01-13 18:06:48 -0300

    Fix mypy issues

--- agent/lm_agent/workload_managers/slurm/reservations.py
+++ agent/lm_agent/workload_managers/slurm/reservations.py
@@ -1,9 +1,9 @@
 """Slurm reservation interface."""
 import asyncio
 import shlex
-from typing import Optional
+from typing import Union
 
-from lm_agent.config import RESERVATION_IDENTIFIER
+from lm_agent.config import settings
 from lm_agent.logs import logger
 from lm_agent.utils import run_command
 from lm_agent.workload_managers.slurm.common import CMD_TIMEOUT, SCONTROL_PATH
@@ -23,7 +23,7 @@ async def scontrol_create_reservation(licenses: str, duration: str) -> bool:
         "create",
         "reservation",
         "user=license-manager",
-        f"reservation={RESERVATION_IDENTIFIER}",
+        f"reservation={settings.RESERVATION_IDENTIFIER}",
         "starttime=now",
         f"duration={duration}",
         "flags=license_only",
@@ -33,7 +33,7 @@ async def scontrol_create_reservation(licenses: str, duration: str) -> bool:
     logger.debug(f"#### Creating reservation for {licenses} with duration {duration} ####")
     reservation_output = await run_command(shlex.join(cmd))
 
-    if not reservation_output or f"Reservation created: {RESERVATION_IDENTIFIER}" not in reservation_output:
+    if reservation_output != f"Reservation created: {settings.RESERVATION_IDENTIFIER}":
         logger.error(f"#### Failed to create reservation ####")
         return False
 
@@ -41,7 +41,7 @@ async def scontrol_create_reservation(licenses: str, duration: str) -> bool:
     return True
 
 
-async def scontrol_read_reservation() -> Optional[Str]:
+async def scontrol_read_reservation() -> Union[str, bool]:
     """
     Read reservation from the cluster.
 
@@ -51,17 +51,20 @@ async def scontrol_read_reservation() -> Optional[Str]:
         SCONTROL_PATH,
         "show",
         "reservation",
-        RESERVATION_IDENTIFIER,
+        settings.RESERVATION_IDENTIFIER,
     ]
 
-    logger.debug(f"#### Getting reservation {RESERVATION_IDENTIFIER} ####")
-    reservation_output = run_command(shlex.join(cmd))
+    logger.debug(f"#### Getting reservation {settings.RESERVATION_IDENTIFIER} ####")
+    reservation_output = await run_command(shlex.join(cmd))
 
-    if not reservation_output or f"Reservation {RESERVATION_IDENTIFIER} not found" in reservation_output:
-        logger.error(f"#### Failed to read reservation {RESERVATION_IDENTIFIER} ####")
-        return None
+    if (
+        not reservation_output
+        or reservation_output == f"Reservation {settings.RESERVATION_IDENTIFIER} not found"
+    ):
+        logger.error(f"#### Failed to read reservation {settings.RESERVATION_IDENTIFIER} ####")
+        return False
 
-    logger.debug(f"#### Successfully read reservation {RESERVATION_IDENTIFIER}####")
+    logger.debug(f"#### Successfully read reservation {settings.RESERVATION_IDENTIFIER}####")
     return reservation_output
 
 
@@ -78,15 +81,15 @@ async def scontrol_update_reservation(licenses: str, duration: str) -> bool:
         SCONTROL_PATH,
         "update",
         "reservation",
-        f"ReservationName={RESERVATION_IDENTIFIER}",
+        f"ReservationName={settings.RESERVATION_IDENTIFIER}",
         f"duration={duration}",
         f"licenses={licenses}",
     ]
 
-    logger.debug(f"#### Updating reservation {RESERVATION_IDENTIFIER} ####")
+    logger.debug(f"#### Updating reservation {settings.RESERVATION_IDENTIFIER} ####")
     reservation_output = await run_command(shlex.join(cmd))
 
-    if not reservation_output or "Reservation updated." not in reservation_output:
+    if reservation_output != "Reservation updated.":
         logger.error(f"#### Failed to update reservation ####")
         return False
 
@@ -103,10 +106,10 @@ async def scontrol_delete_reservation() -> bool:
     cmd = [
         SCONTROL_PATH,
         "delete",
-        f"ReservationName={RESERVATION_IDENTIFIER}",
+        f"ReservationName={settings.RESERVATION_IDENTIFIER}",
     ]
 
-    logger.debug(f"#### Deleting reservation {RESERVATION_IDENTIFIER} ####")
+    logger.debug(f"#### Deleting reservation {settings.RESERVATION_IDENTIFIER} ####")
     reservation_output = await run_command(shlex.join(cmd))
 
     # Reservation delete command doesn't output any message on success
