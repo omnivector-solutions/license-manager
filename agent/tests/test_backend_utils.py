5304c1369aba990384f073f33d67acfc625e9f21
Juliana Karoline <julianaklulo@gmail.com>
2023-03-03 15:17:35 -0300

    Change docstring in test

--- agent/tests/test_backend_utils.py
+++ agent/tests/test_backend_utils.py
@@ -28,7 +28,7 @@ def test__write_token_to_cache__caches_a_token(mock_cache_dir):
     assert token_path.exists()
     assert token_path.read_text() == "dummy-token"
 
-    # Assert that the permissions for the file are read/write for the current user
+    # Assert that the permissions for the file are read/write only for the current user
     assert token_path.stat().st_mode & (stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO) == 0o600
 
 
