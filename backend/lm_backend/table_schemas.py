e4e63a0a2e3e62e441e75ddb8bd4db058f39693c
Juliana Karoline <julianaklulo@gmail.com>
2023-03-21 11:57:57 -0300

    Update table schemas

--- backend/lm_backend/table_schemas.py
+++ backend/lm_backend/table_schemas.py
@@ -1,94 +1,147 @@
 """
-Table schema for tables in the license-manager backend
+Table schema for tables in the License Manager API.
 """
+
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, Table, func
 from sqlalchemy.sql.schema import CheckConstraint, ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime
-from sqlalchemy_utils import ScalarListType  # type: ignore
 
 metadata = sqlalchemy.MetaData()
 
-
-license_table = Table(
-    "license",
+license_servers_table = Table(
+    "license_servers",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("product_feature", String, unique=True),
-    Column("used", Integer, CheckConstraint("used>=0")),
-    Column("total", Integer, CheckConstraint("total>=0")),
-    CheckConstraint("used<=total"),
+    Column("host", String, nullable=False),
+    Column("port", Integer, CheckConstraint("port>=0"), nullable=False),
 )
 
-license_searchable_fields = [
-    license_table.c.product_feature,
+license_servers_searchable_fields = [
+    license_servers_table.c.host,
+    license_servers_table.c.port,
 ]
 
-license_sortable_fields = [
-    license_table.c.product_feature,
-    license_table.c.used,
-    license_table.c.total,
+license_servers_sortable_fields = [
+    license_servers_table.c.host,
 ]
 
-
-booking_table = Table(
-    "booking",
+clusters_table = Table(
+    "clusters",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("job_id", String),
-    Column("product_feature", String),
-    Column("booked", Integer, CheckConstraint("booked>=0")),
-    Column("lead_host", String),
-    Column("user_name", String),
-    Column("cluster_name", String),
+    Column("name", String, nullable=False),
+    Column("client_id", String, nullable=False),
+)
+
+clusters_searchable_fields = [
+    clusters_table.c.name,
+    clusters_table.c.client_id,
+]
+
+clusters_sortable_fields = [
+    clusters_table.c.name,
+]
+
+jobs_table = Table(
+    "jobs",
+    metadata,
+    Column("slurm_id", Integer, primary_key=True, autoincrement=False),
+    Column("cluster_id", Integer, ForeignKey("clusters.id"), nullable=False),
+    Column("username", String, nullable=False),
+    Column("lead_host", String, nullable=False),
+)
+
+jobs_searchable_fields = [
+    jobs_table.c.slurm_id,
+]
+
+jobs_sortable_fields = [
+    jobs_table.c.slurm_id,
+]
+
+configs_table = Table(
+    "configs",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("cluster_id", Integer, ForeignKey("clusters.id"), nullable=False),
+    Column("grace_time", Integer, CheckConstraint("grace_time>=0"), nullable=False),
+    Column("reserved", Integer, CheckConstraint("reserved>=0"), nullable=False),
+    Column("type", String, nullable=False),
+)
+
+configs_searchable_fields = [
+    configs_table.c.name,
+]
+
+configs_sortable_fields = [
+    configs_table.c.name,
+]
+
+license_servers_configs = Table(
+    "license_servers_configs",
+    metadata,
+    Column("license_server_id", Integer, ForeignKey("license_servers.id"), nullable=False),
+    Column("config_id", Integer, ForeignKey("configs.id"), nullable=False),
+)
+
+products_table = Table(
+    "products",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+)
+
+products_searchable_fields = [
+    products_table.c.name,
+]
+
+products_sortable_fields = [
+    products_table.c.name,
+]
+
+features_table = Table(
+    "features",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("product_id", Integer, ForeignKey("products.id"), nullable=False),
+    Column("config_id", Integer, ForeignKey("configs.id"), nullable=False),
+)
+
+features_searchable_fields = [
+    features_table.c.name,
+]
+
+features_sortable_fields = [
+    features_table.c.name,
+]
+
+inventories_table = Table(
+    "inventories",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("feature_id", Integer, ForeignKey("features.id"), nullable=False),
+    Column("total", Integer, CheckConstraint("total>=0"), nullable=False),
+    Column("used", Integer, CheckConstraint("used>=0"), nullable=False),
+)
+
+bookings_table = Table(
+    "bookings",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("job_id", Integer, ForeignKey("jobs.slurm_id"), nullable=False),
+    Column("feature_id", Integer, ForeignKey("features.id"), nullable=False),
+    Column("quantity", Integer, CheckConstraint("quantity>=0"), nullable=False),
     Column("created_at", DateTime, default=func.now()),
-    Column("config_id", Integer, ForeignKey("config.id"), nullable=False),
+    Column("config_id", Integer, ForeignKey("configs.id"), nullable=False),
 )
 
-booking_searchable_fields = [
-    booking_table.c.job_id,
-    booking_table.c.product_feature,
-    booking_table.c.lead_host,
-    booking_table.c.user_name,
-    booking_table.c.cluster_name,
+bookings_searchable_fields = [
+    bookings_table.c.job_id,
 ]
 
-booking_sortable_fields = [
-    booking_table.c.job_id,
-    booking_table.c.product_feature,
-    booking_table.c.booked,
-    booking_table.c.lead_host,
-    booking_table.c.user_name,
-    booking_table.c.cluster_name,
-]
-
-
-config_table = Table(
-    "config",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String, unique=True),
-    Column("product", String),
-    Column("features", String),
-    Column("license_servers", ScalarListType(str)),
-    Column("license_server_type", String),
-    Column("grace_time", Integer, CheckConstraint("grace_time>=0")),
-    Column("client_id", String, nullable=False, index=True),
-)
-
-config_searchable_fields = [
-    config_table.c.name,
-    config_table.c.product,
-    config_table.c.features,
-    config_table.c.license_server_type,
-    config_table.c.client_id,
-]
-
-config_sortable_fields = [
-    config_table.c.name,
-    config_table.c.product,
-    config_table.c.features,
-    config_table.c.license_server_type,
-    config_table.c.grace_time,
-    config_table.c.client_id,
+bookings_sortable_fields = [
+    bookings_table.c.job_id,
 ]
