2141ffc2056f7cfd1311fd00372fa885bb55b7fa
Juliana Karoline <julianaklulo@gmail.com>
2023-03-28 11:18:57 -0300

    Lint code

--- backend/lm_backend/api/routes/license_server.py
+++ backend/lm_backend/api/routes/license_server.py
@@ -1,18 +1,25 @@
-from typing import List, Optional
+from typing import List
 
-from fastapi import APIRouter, Body, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from lm_backend.api.crud.license_server import LicenseServerCRUD
-from lm_backend.api.schemas.license_server import LicenseServerCreateSchema, LicenseServerUpdateSchema, LicenseServerSchema
+from lm_backend.api.schemas.license_server import (
+    LicenseServerCreateSchema,
+    LicenseServerSchema,
+    LicenseServerUpdateSchema,
+)
 from lm_backend.database import get_session
-from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
 crud = LicenseServerCRUD()
 
 
-@router.post("/", response_model=LicenseServerSchema, status_code=status.HTTP_201_CREATED,
+@router.post(
+    "/",
+    response_model=LicenseServerSchema,
+    status_code=status.HTTP_201_CREATED,
 )
 async def create_license_server(
     license_server: LicenseServerCreateSchema,
@@ -29,13 +36,11 @@ async def read_all_license_servers(db_session: AsyncSession = Depends(get_sessio
 
 
 @router.get("/{license_server_id}", response_model=LicenseServerSchema, status_code=status.HTTP_200_OK)
-async def read_license_server(
-    license_server_id: int, db_session: AsyncSession = Depends(get_session)
-):
+async def read_license_server(license_server_id: int, db_session: AsyncSession = Depends(get_session)):
     """Return a license server with the given id."""
     return await crud.read(db_session=db_session, license_server_id=license_server_id)
 
-    
+
 @router.put("/{license_server_id}", response_model=LicenseServerSchema, status_code=status.HTTP_200_OK)
 async def update_license_server(
     license_server_id: int,
@@ -43,13 +48,15 @@ async def update_license_server(
     db_session: AsyncSession = Depends(get_session),
 ):
     """Update a license server in the database."""
-    return await crud.update(db_session=db_session, license_server_id=license_server_id, license_server_update=license_server_update)
+    return await crud.update(
+        db_session=db_session,
+        license_server_id=license_server_id,
+        license_server_update=license_server_update,
+    )
 
 
 @router.delete("/{license_server_id}", status_code=status.HTTP_200_OK)
-async def delete_license_server(
-    license_server_id: int, db_session: AsyncSession = Depends(get_session)
-):
+async def delete_license_server(license_server_id: int, db_session: AsyncSession = Depends(get_session)):
     """Delete a license server from the database."""
     await crud.delete(db_session=db_session, license_server_id=license_server_id)
     return {"message": "License server deleted successfully"}
