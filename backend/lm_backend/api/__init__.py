519c46f8e8c068c1612cb45bcd80367b88558afe
Juliana Karoline <julianaklulo@gmail.com>
2023-03-27 22:10:10 -0300

    Add license_server router

--- backend/lm_backend/api/__init__.py
+++ backend/lm_backend/api/__init__.py
@@ -1,10 +1,5 @@
 from fastapi import APIRouter
+from lm_backend.api.license_server.routes import router as router_license_server
 
-from lm_backend.api.booking import router as router_booking
-from lm_backend.api.config import router as router_config
-from lm_backend.api.license import router as router_license
-
-api_v1 = APIRouter()
-api_v1.include_router(router_license, prefix="/license", tags=["License"])
-api_v1.include_router(router_booking, prefix="/booking", tags=["Booking"])
-api_v1.include_router(router_config, prefix="/config", tags=["Config"])
+api = APIRouter()
+api.include_router(router_license_server, prefix="/license_servers", tags=["License Server"])
