4e927589eeb46b2c45f0995e838aa70c11477ed8
Juliana Karoline <julianaklulo@gmail.com>
2023-03-28 12:29:52 -0300

    Include cluster router to API

--- backend/lm_backend/api/__init__.py
+++ backend/lm_backend/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from lm_backend.api.routes.cluster import router as router_cluster
 from lm_backend.api.routes.license_server import router as router_license_server
 
 api = APIRouter()
 api.include_router(router_license_server, prefix="/license_servers", tags=["License Server"])
+api.include_router(router_cluster, prefix="/clusters", tags=["Cluster"])
