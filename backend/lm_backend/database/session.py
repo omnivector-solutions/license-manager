2086872803bdde28ee71a8f262cd331861fdbfde
Juliana Karoline <julianaklulo@gmail.com>
2023-03-28 09:26:15 -0300

    Update database module

--- backend/lm_backend/database/session.py
+++
@@ -1,9 +0,0 @@
-"""Database session."""
-from lm_backend.database.storage import async_sessionmaker
-from sqlalchemy.ext.asyncio import AsyncSession
-
-
-async def get_session() -> AsyncSession:
-    """Create a new async session for each request."""
-    async with async_sessionmaker() as session:
-        yield session
