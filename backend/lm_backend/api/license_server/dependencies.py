a6614f28edb3e2b744ed704c456eea3dae0569c0
Juliana Karoline <julianaklulo@gmail.com>
2023-03-28 09:27:55 -0300

    Remove unused files

--- backend/lm_backend/api/license_server/dependencies.py
+++
@@ -1,8 +0,0 @@
-from lm_backend.api.license_server.crud import LicenseServerCRUD
-from lm_backend.database.storage import async_session
-
-
-async def get_license_server_crud() -> LicenseServerCRUD:
-    async with async_session() as session:
-        async with session.begin():
-            yield LicenseServerCRUD(async_session=session)
