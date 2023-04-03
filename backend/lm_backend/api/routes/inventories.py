ed999698692f6588e0f7a275281bda5de0cd88cf
Juliana Karoline <julianaklulo@gmail.com>
2023-04-03 15:59:46 -0300

    Add inventories routes

---
+++ backend/lm_backend/api/routes/inventories.py
@@ -0,0 +1,66 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from lm_backend.api.cruds.generic import GenericCRUD
+from lm_backend.api.models import Inventory
+from lm_backend.api.schemas import (
+    InventoryCreateSchema,
+    InventorySchema,
+    InventoryUpdateSchema,
+    InventoryCreateSchema,
+    InventoryUpdateSchema,
+)
+from lm_backend.database import get_session
+
+router = APIRouter()
+
+
+crud_inventory = GenericCRUD(Inventory, InventoryCreateSchema, InventoryUpdateSchema)
+
+
+@router.post(
+    "/",
+    response_model=InventorySchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_inventory(
+    inventory: InventoryCreateSchema,
+    db_session: AsyncSession = Depends(get_session),
+):
+    """Create a new inventory."""
+    return await crud_inventory.create(db_session=db_session, obj=inventory)
+
+
+@router.get("/", response_model=List[InventorySchema], status_code=status.HTTP_200_OK)
+async def read_all_inventories(db_session: AsyncSession = Depends(get_session)):
+    """Return all inventories."""
+    return await crud_inventory.read_all(db_session=db_session)
+
+
+@router.get("/{inventory_id}", response_model=InventorySchema, status_code=status.HTTP_200_OK)
+async def read_inventory(inventory_id: int, db_session: AsyncSession = Depends(get_session)):
+    """Return a inventory for a feature."""
+    return await crud_inventory.read(db_session=db_session, id=inventory_id)
+
+
+@router.put("/{inventory_id}", response_model=InventorySchema, status_code=status.HTTP_200_OK)
+async def update_inventory(
+    inventory_id: int,
+    inventory_update: InventoryUpdateSchema,
+    db_session: AsyncSession = Depends(get_session),
+):
+    """Update an inventory in the database."""
+    return await crud_inventory.update(
+        db_session=db_session,
+        id=inventory_id,
+        obj=inventory_update,
+    )
+
+
+@router.delete("/{inventory_id}", status_code=status.HTTP_200_OK)
+async def delete_inventory(inventory_id: int, db_session: AsyncSession = Depends(get_session)):
+    """Delete an inventory from the database."""
+    await crud_inventory.delete(db_session=db_session, id=inventory_id)
+    return {"message": "Inventory deleted successfully"}
