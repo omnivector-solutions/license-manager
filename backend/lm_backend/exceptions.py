968775a8778e627ec72bac3011045485786fd6b9
Juliana Karoline <julianaklulo@gmail.com>
2023-05-19 11:56:26 -0300

    Remove unused files

--- backend/lm_backend/exceptions.py
+++
@@ -1,8 +0,0 @@
-"""
-Custom exceptions for the License Manager Backend.
-"""
-from buzz import Buzz
-
-
-class LicenseManagerFeatureConfigurationIncorrect(Buzz):
-    """Exception for feature configuration incorrectly formatted."""
