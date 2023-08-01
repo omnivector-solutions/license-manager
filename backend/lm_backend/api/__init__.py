45624f8534e1a5641e363110e6ee29fbbcf145be
Juliana Karoline <julianaklulo@gmail.com>
2023-08-01 13:34:30 -0300

    Remove clusters

--- backend/lm_backend/api/__init__.py
+++ backend/lm_backend/api/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from lm_backend.api.routes.bookings import router as router_bookings
-from lm_backend.api.routes.clusters import router as router_clusters
 from lm_backend.api.routes.configurations import router as router_configurations
 from lm_backend.api.routes.features import router as router_features
 from lm_backend.api.routes.jobs import router as router_jobs
@@ -9,7 +8,6 @@ from lm_backend.api.routes.license_servers import router as router_license_serve
 from lm_backend.api.routes.products import router as router_products
 
 api = APIRouter()
-api.include_router(router_clusters, prefix="/clusters", tags=["Cluster"])
 api.include_router(router_configurations, prefix="/configurations", tags=["Configuration"])
 api.include_router(router_license_servers, prefix="/license_servers", tags=["License Server"])
 api.include_router(router_products, prefix="/products", tags=["Product"])
