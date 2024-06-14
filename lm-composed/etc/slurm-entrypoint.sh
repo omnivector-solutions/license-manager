6e3c9a898469a8852e817195f7142148100cf004
Juliana Karoline <julianaklulo@gmail.com>
2024-06-14 12:03:49 -0300

    ASP-5024 Start structuring lm-composed

---
+++ lm-composed/etc/slurm-entrypoint.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+
+
+echo "---> Starting the MUNGE Authentication service (munged) ..."
+service munge start
+
+if [ "$1" = "slurmdbd" ]
+then
+    echo "---> Starting the Slurm Database Daemon (slurmdbd) ..."
+    {
+        . /etc/slurm/slurmdbd.conf
+        until echo "SELECT 1" | mysql -h $StorageHost -u$StorageUser -p$StoragePass 2>&1 > /dev/null
+        do
+            echo "-- Waiting for database to become active ..."
+            sleep 2
+        done
+    }
+    echo "-- Database is now active ..."
+
+    exec gosu slurm /usr/sbin/slurmdbd -Dvvv
+fi
+
+if [ "$1" = "slurmctld" ]
+then
+    echo "---> Waiting for slurmdbd to become active before starting slurmctld ..."
+
+    until 2>/dev/null >/dev/tcp/slurmdbd/6819
+    do
+        echo "-- slurmdbd is not available.  Sleeping ..."
+        sleep 2
+    done
+    echo "-- slurmdbd is now active ..."
+
+    echo "---> Starting the Slurm Controller Daemon (slurmctld) ..."
+    exec gosu slurm /usr/sbin/slurmctld -Dvvv
+fi
+
+if [ "$1" = "slurmd" ]
+then
+    echo "---> Waiting for slurmctld to become active before starting slurmd..."
+
+    until 2>/dev/null >/dev/tcp/slurmctld/6817
+    do
+        echo "-- slurmctld is not available.  Sleeping ..."
+        sleep 2
+    done
+    echo "-- slurmctld is now active ..."
+
+    echo "---> Starting the Slurm Node Daemon (slurmd) ..."
+    exec /usr/sbin/slurmd -Dvvv
+fi
+
+if [ "$1" = "slurmrestd" ]
+then
+
+    echo "---> Waiting for slurmctld to become active before starting slurmrestd..."
+
+    until 2>/dev/null >/dev/tcp/slurmctld/6817
+    do
+        echo "-- slurmctld is not available.  Sleeping ..."
+        sleep 2
+    done
+    echo "-- slurmctld is now active ..."
+
+    echo "---> Starting the Slurm Rest API (slurmrestd) ..."
+    exec /usr/sbin/slurmrestd -vvvv -a rest_auth/jwt 0.0.0.0:6820
+fi
+
+exec "$@"
