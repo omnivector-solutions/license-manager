f4699bddb60257d2442cd17a8082dd66798bc645
Juliana Karoline <julianaklulo@gmail.com>
2024-07-23 16:24:55 -0300

    ASP-5025 Run tests in docker-compose

--- lm-simulator/lm_simulator/config.py
+++ lm-simulator/lm_simulator/config.py
@@ -8,6 +8,12 @@ class Settings(BaseSettings):
     DATABASE_NAME: str = "local-db"
     DATABASE_PORT: int = 5432
 
+    TEST_DATABASE_HOST: str = "localhost"
+    TEST_DATABASE_USER: str = "test-db-user"
+    TEST_DATABASE_PSWD: str = "test-db-pswd"
+    TEST_DATABASE_NAME: str = "test-db-name"
+    TEST_DATABASE_PORT: int = 5433
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
