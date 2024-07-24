3538f00dbae4a7629ef13905e23aa9617b68e4c7
Juliana Karoline <julianaklulo@gmail.com>
2024-07-24 10:53:42 -0300

    ASP-5025 Update job application example

--- lm-simulator/job/application.sh
+++ lm-simulator/job/application.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-# You must modify this value to reflect the ip address and port that the
+# You must modify this value to reflect the IP address and port that the
 # license-manager-simulator is listening on in your environment.
 #
 # The format of the value is: `http://<ip-address>:<port>`
 URL="http://localhost:8000"
 
-# number of licenses: from the CLI or 42
-if [ -z $1 ]; then
-	X=42
+# Number of licenses: from the CLI or default to 42
+if [ -z "$1" ]; then
+    X=42
 else
-	X=$1
+    X=$1
 fi
 
 payload="{\"quantity\": "$X", \
@@ -20,24 +20,28 @@ payload="{\"quantity\": "$X", \
           \"license_name\": \"test_feature\"}"
 
 echo "Requesting $X licenses for user $USER"
-status=$(curl -s -o /dev/null -w '%{http_code}' \
-	      -X 'POST' \
-	      -H 'Content-Type: application/json' \
-	      -d "$payload" \
-	      "$URL"/lm-sim/licenses-in-use/)
-if [ "$status" = "201" ]; then
-	echo "There are enought licenses available, lets run (sleep) the job"
-	sleep $((RANDOM % 100 + 100))
+response=$(curl -s -w 'HTTPSTATUS:%{http_code}' \
+              -X 'POST' \
+              -H 'Content-Type: application/json' \
+              -d "$payload" \
+              "$URL"/lm-sim/licenses-in-use)
+
+response_status=$(echo "$response" | sed -e 's/.*HTTPSTATUS://')
+response_body=$(echo "$response" | sed -e 's/HTTPSTATUS:.*//')
+
+if [ "$response_status" = "201" ]; then
+    echo "There are enough licenses available, let's run (sleep) the job"
+    sleep $((RANDOM % 100 + 100))
 else
-	echo "There are not enough licenses, let's crash the job"
-	tail /NOT_ENOUGH_LICENSES
+    echo "There are not enough licenses, let's crash the job"
+    tail /NOT_ENOUGH_LICENSES
 fi
 
-echo "Puting the licenses back"
+license_in_use_id=$(echo "$response_body" | grep -o '"id":[0-9]*' | awk -F: '{print $2}')
+
+echo "Putting the licenses back"
 curl -s -o /dev/null \
      -X DELETE \
-     -H 'Content-Type: application/json' \
-     -d "$payload" \
-     "$URL"/lm-sim/licenses-in-use/
+     "$URL/lm-sim/licenses-in-use/$license_in_use_id"
 
 echo "Job done"
