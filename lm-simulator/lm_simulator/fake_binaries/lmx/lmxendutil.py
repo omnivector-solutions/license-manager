a78b2a6544b1ea11cfd9267bcae0cd10224a59d2
Juliana Karoline <julianaklulo@gmail.com>
2024-08-07 21:43:32 -0300

    ASP-4668 Update lmxendutil script

--- lm-simulator/lm_simulator/fake_binaries/lmx/lmxendutil.py
+++ lm-simulator/lm_simulator/fake_binaries/lmx/lmxendutil.py
@@ -5,50 +5,65 @@ File that will be called by the license-manager-agent in the report function.
 It will hit the /licenses-in-use/ endpoint and will generate the report in the same format as the LM-X,
 this way we can use the same LM-X parser in the license-manager-agent.
 """
+import sys
+from pathlib import Path
+
 import requests
 from jinja2 import Environment, FileSystemLoader
 
-# You must modify this value to reflect the ip address and port that the
-# license-manager-simulator is listening on in your environment.
-#
-# The format of the value is: `http://<ip-address>:<port>`
-URL = "http://localhost:8000"
 
-
-def get_server_data():
+def get_server_data(lm_sim_host: str, lm_sim_port: str) -> dict:
     """
-    To simulate the LM-X output, add this license to the backend:
+    To simulate the LM-X output, add a license with ``lmx`` license server type to the backend:
     {
         "name": "HyperWorks",
         "total": 1000
+        "license_server_type": "lmx"
     }
     Since LM-X outputs only the ``feature`` name (omitting the ``product``), the license
     in the simulator database should be created with the feature as its name.
     """
-    licenses = requests.get(URL + "/lm-sim/licenses/").json()
+    licenses = requests.get(f"http://{lm_sim_host}:{lm_sim_port}/lm-sim/licenses/type/lmx").json()
 
-    for license in licenses:
-        if license["name"] == "HyperWorks":
-            any_in_use = license["in_use"] > 0
-            return {
+    return {
+        "licenses": [
+            {
                 "license_name": license.get("name"),
                 "total_licenses": license.get("total"),
                 "in_use": license.get("in_use"),
                 "licenses_in_use": license.get("licenses_in_use"),
-                "any_in_use": any_in_use,
             }
+            for license in licenses
+        ]
+    }
 
 
-def generate_license_server_output():
+def generate_license_server_output(license_information: dict) -> None:
     """Print output formatted to stdout."""
+    script_path = Path(__file__).resolve()
+    template_dir = script_path.parent
     source = "lmxendutil.out.tmpl"
-    license_information = get_server_data()
 
-    template = Environment(loader=FileSystemLoader("."), trim_blocks=True, lstrip_blocks=True).get_template(
-        source
-    )
+    template = Environment(loader=FileSystemLoader(str(template_dir))).get_template(source)
     print(template.render(**license_information))
 
 
+def main():
+    """
+    License Manager Agent will invoke the lmxendutil binary with the following arguments:
+    ```
+    lmxendutil -licstat -host <host> -port <port>
+    ```
+    The license server host and port will be identify the License Manager Simulator API.
+    """
+    assert len(sys.argv) == 6, "Invalid number of arguments"
+
+    lm_sim_host = sys.argv[3]
+    lm_sim_port = sys.argv[5]
+
+    license_information = get_server_data(lm_sim_host, lm_sim_port)
+    generate_license_server_output(license_information)
+
+
 if __name__ == "__main__":
-    generate_license_server_output()
+    main()
