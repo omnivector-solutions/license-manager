869355a6971dccb98e30d42c00b0c06a8b1253ee
Juliana Karoline de Sousa <julianaklulo@gmail.com>
2024-08-19 18:02:48 -0300

    Add lm-simulator-api to check-prereqs script (#379)

--- release-scripts/check-prereqs.sh
+++ release-scripts/check-prereqs.sh
@@ -2,7 +2,7 @@
 
 BASE_BRANCH="main"
 SUBPROJECT=${PWD##*/}
-ALL_SUBPROJECTS="lm-agent lm-api lm-cli lm-simulator"
+ALL_SUBPROJECTS="lm-agent lm-api lm-cli lm-simulator lm-simulator-api"
 
 function bail {
     echo "!!! $1 -- Aborting !!!"
